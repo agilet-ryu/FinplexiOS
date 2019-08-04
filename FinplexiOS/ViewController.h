87b8fb2a3172d044fc2b41ee2fd3300648965db6
agilet-ryu <haku.ryu@agile-t.com>
2019-08-04 12:28:09 +0800

    Initial Commit

---
+++ FinplexiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FinplexiOS
+//
+//  Created by agilet-ryu on 2019/8/4.
+//  Copyright © 2019 Fujitsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FinplexiOSTests/FinplexiOSTests.m
@@ -0,0 +1,37 @@
+//
+//  FinplexiOSTests.m
+//  FinplexiOSTests
+//
+//  Created by agilet-ryu on 2019/8/4.
+//  Copyright © 2019 Fujitsu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FinplexiOSTests : XCTestCase
+
+@end
+
+@implementation FinplexiOSTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
